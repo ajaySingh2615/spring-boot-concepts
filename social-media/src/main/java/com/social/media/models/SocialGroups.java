d921492706650b1ac0fe92fd398837881d445eba
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-24 13:19:25 +0530

    done social-media application

--- social-media/src/main/java/com/social/media/models/SocialGroups.java
+++ social-media/src/main/java/com/social/media/models/SocialGroups.java
@@ -1,11 +1,17 @@
 package com.social.media.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SocialGroups {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
