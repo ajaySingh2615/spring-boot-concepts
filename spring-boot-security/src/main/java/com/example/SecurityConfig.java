36c54b3659a17613d863897394446ae6b87bc709
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-26 14:16:23 +0530

    password encoding and hashing password

--- spring-boot-security/src/main/java/com/example/SecurityConfig.java
+++ spring-boot-security/src/main/java/com/example/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -44,11 +46,11 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")
+                .password(passwordEncoder().encode("password1"))
                 .roles("USER")
                 .build();
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}password2")
+                .password(passwordEncoder().encode("password2"))
                 .roles("ADMIN")
                 .build();
 
@@ -57,5 +59,10 @@ public class SecurityConfig {
         userDetailsManager.createUser(user1);
         return userDetailsManager;
 //        return new InMemoryUserDetailsManager(user1, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
